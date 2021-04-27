30163e30870dbdd2b87f0147d2c2fb5e39a8864c
The Guy with Glasses <codewithchin@gmail.com>
2021-04-27 17:00:18 +0530

    Enhanced Look and Feel

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -38,14 +38,14 @@ export default function Teams() {
                     <div className="carousel-inner">
                         <div className="carousel-item active">
                             <div className="team-cards">
-                                <TCard avatar="https://avatars.githubusercontent.com/u/38172685?v=4" heading="CHINMAY MULAY" img="arrow-bold-top" text="President(Lead)" text1="'Miles to go before I sleep.'" linkedin="https://www.linkedin.com/in/cmulay17/" twitter="https://twitter.com/cmulay17?s=09" github="https://github.com/cmulay" />
-                                <TCard avatar="https://avatars.githubusercontent.com/u/67738204?v=4" heading="DAVE BHANDARI" img="connect" text="Vice-President(Co-Lead)" text1="'Student | Self Taught Developer'" linkedin="https://linkedin.com/in/dave-bhandari-4a74761a7/" twitter="https://twitter.com/dave_bhandari" github="https://github.com/Davekibh" />
+                                <TCard avatar="https://avatars.githubusercontent.com/u/38172685?v=4" heading="CHINMAY MULAY" img="arrow-bold-top" text="Community Lead" text1="Miles to go before I sleep." linkedin="https://www.linkedin.com/in/cmulay17/" twitter="https://twitter.com/cmulay17?s=09" github="https://github.com/cmulay" />
+                                <TCard avatar="https://avatars.githubusercontent.com/u/67738204?v=4" heading="DAVE BHANDARI" img="connect" text="Android Lead" text1="Student | Self Taught Developer" linkedin="https://linkedin.com/in/dave-bhandari-4a74761a7/" twitter="https://twitter.com/dave_bhandari" github="https://github.com/Davekibh" />
                             </div>
                         </div>
                         <div className="carousel-item">
                             <div className="team-cards">
-                                <TCard avatar="https://avatars.githubusercontent.com/u/64542454?v=4" heading="BEDANT HOTA" text="Graphics President(Co-Lead)" text1="'Quite a Nerd, but knows how to code.:)'" linkedin="https://www.linkedin.com/in/bedant-hota-99a5371a5/" twitter="https://twitter.com/BedantHota" github="https://github.com/bedantH" />
-                                <TCard avatar="https://avatars.githubusercontent.com/u/72993471?v=4" heading="PRATHAM YADAV" text="Co-Lead" text1="'Student | Developer'" linkedin="https://www.linkedin.com/in/pratham-yadav-0612531ba/" twitter="https://twitter.com/Pratham06697358" github="https://github.com/ypratham" />
+                                <TCard avatar="https://avatars.githubusercontent.com/u/64542454?v=4" heading="BEDANT HOTA" text="Web Lead" text1="Quite a Nerd, but knows how to code.:)" linkedin="https://www.linkedin.com/in/bedant-hota-99a5371a5/" twitter="https://twitter.com/BedantHota" github="https://github.com/bedantH" />
+                                <TCard avatar="https://avatars.githubusercontent.com/u/72993471?v=4" heading="PRATHAM YADAV" text="Web Co-Lead" text1="Student | Developer" linkedin="https://www.linkedin.com/in/pratham-yadav-0612531ba/" twitter="https://twitter.com/Pratham06697358" github="https://github.com/ypratham" />
                             </div>
                         </div>
                     </div>
